9db9f6fb034849d0f0883b118bed7051377f5ec5
SimpleDevSam <129802221+SimpleDevSam@users.noreply.github.com>
2023-04-03 20:45:27 -0300

    Add files via upload

--- Player.js
+++ Player.js
@@ -7,6 +7,8 @@ export class Player {
     legslenght;
     gluteslenght;
 
+//Defining constructor and logic to stablish lenght atributtes
+
     constructor(name,age,shape) {
         this.name = name;
         this.age = age;
@@ -30,8 +32,121 @@ export class Player {
         }
     }
 
+    //Defining getter to retrieve player status
+
     get status () {
-        return [this.chestlenght, this.armslenght, this.legslenght, this.gluteslenght]
+        return console.log([this.chestlenght, this.armslenght, this.legslenght, this.gluteslenght]);
     }
+
+     //Creating the method which train chest and boost their chest status
+
+    chestTraining(intensity){
+
+        if (intensity !== "L" && intensity !=="H" ){
+            return console.log("Sorry, you have to type\n [L] for Light \n [H] for Heavy");
+
+        } else if (intensity == "L") {
+            let previousLenght = this.chestlenght;
+            this.chestlenght = (this.chestlenght * 1.002).toFixed(2);
+            let sizeGained = (this.chestlenght*0.002).toFixed(2);
+
+            return console.log("\nYou've chosen to train Chest! Good for you!\n Previous Lenght :"+
+             previousLenght +" \n Gained "+ sizeGained + 
+            " of Chess Lenght! \n Now you have " + this.chestlenght +"cm²");
+
+        } else {
+
+            let previousLenght = this.chestlenght;
+            this.chestlenght = (this.chestlenght * 1.005).toFixed(2);
+            let sizeGained = (this.chestlenght*0.005).toFixed(2);
+
+            return console.log("\nYou've chosen to train Chest! Good for you!\n Previous Lenght :"+ previousLenght +" \n Gained "+ 
+            sizeGained + 
+            " of Chess Lenght! \n Now you have " + this.chestlenght +"cm²")
+        }
+
+    }
+
+    //Creating the method which train arms and boost their chest status
+
+    armsTraining(intensity){
+
+        if (intensity !== "L" && intensity !=="H" ){
+            return console.log("Sorry, you have to type\n [L] for Light \n [H] for Heavy");
+
+        } else if (intensity == "L") {
+            let previousLenght = this.armslenght;
+            this.armslenght = (this.armslenght * 1.002).toFixed(2);
+            let sizeGained = (this.armslenght*0.002).toFixed(2);
+
+            return console.log("\nYou've chosen to train Chest! Good for you!\n Previous Lenght :"+
+             previousLenght +" \n Gained "+ sizeGained + 
+            " of Chess Lenght! \n Now you have " + this.armslenght +"cm²");
+
+        } else {
+            
+            let previousLenght = this.armslenght;
+            this.armslenght = (this.armslenght * 1.005).toFixed(2);
+            let sizeGained = (this.armslenght*0.005).toFixed(2);
+
+            return console.log("\nYou've chosen to train Chest! Good for you!\n Previous Lenght :"+ previousLenght +" \n Gained "+ 
+            sizeGained + 
+            " of Chess Lenght! \n Now you have " + this.armslenght +"cm²")
+        }
+
+    }
+    glutesTraining(intensity){
+
+        if (intensity !== "L" && intensity !=="H" ){
+            return console.log("Sorry, you have to type\n [L] for Light \n [H] for Heavy");
+
+        } else if (intensity == "L") {
+            let previousLenght = this.gluteslenght;
+            this.gluteslenght = (this.gluteslenght * 1.002).toFixed(2);
+            let sizeGained = (this.gluteslenght*0.002).toFixed(2);
+
+            return console.log("\nYou've chosen to train Chest! Good for you!\n Previous Lenght :"+
+             previousLenght +" \n Gained "+ sizeGained + 
+            " of Chess Lenght! \n Now you have " + this.gluteslenght +"cm²");
+
+        } else {
+            
+            let previousLenght = this.gluteslenght;
+            this.gluteslenght = (this.armslenght * 1.005).toFixed(2);
+            let sizeGained = (this.gluteslenght*0.005).toFixed(2);
+
+            return console.log("\nYou've chosen to train Chest! Good for you!\n Previous Lenght :"+ previousLenght +" \n Gained "+ 
+            sizeGained + 
+            " of Chess Lenght! \n Now you have " + this.gluteslenght +"cm²")
+        }
+
+    }
+    legsTraining(intensity){
+
+        if (intensity !== "L" && intensity !=="H" ){
+            return console.log("Sorry, you have to type\n [L] for Light \n [H] for Heavy");
+
+        } else if (intensity == "L") {
+            let previousLenght = this.legslenght;
+            this.legslenght = (this.legslenght * 1.002).toFixed(2);
+            let sizeGained = (this.legslenght*0.002).toFixed(2);
+
+            return console.log("\nYou've chosen to train Chest! Good for you!\n Previous Lenght :"+
+             previousLenght +" \n Gained "+ sizeGained + 
+            " of Chess Lenght! \n Now you have " + this.legslenght +"cm²");
+
+        } else {
+            
+            let previousLenght = this.legslenght;
+            this.legslenght = (this.armslenght * 1.005).toFixed(2);
+            let sizeGained = (this.legslenght*0.005).toFixed(2);
+
+            return console.log("\nYou've chosen to train Chest! Good for you!\n Previous Lenght :"+ previousLenght +" \n Gained "+ 
+            sizeGained + 
+            " of Chess Lenght! \n Now you have " + this.legslenght +"cm²")
+        }
+
+    }
+
     }
 
--- index.js
+++ index.js
@@ -2,5 +2,12 @@ import { Player } from "./Player.js";
 
 const player1 = new Player ("Samuel", 25, "M");
 
+player1.chestTraining("L");
 
+player1.armsTraining("L");
 
+player1.legsTraining("H");
+
+player1.glutesTraining("H");
+
+player1.status;
